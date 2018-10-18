b784df9edf3b30ec27a470444239d8393bfb2aa6
Adam Šorfa <adam.sorfa@gmail.com>
2018-10-18 10:24:24 +0000

    create new and edit forms and partials

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,5 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap-sprocketss
 //= require turbolinks
 //= require_tree .
---
+++ app/views/recipes/index.html.rb
@@ -0,0 +1,2 @@
+<h1>Hallo World this is a index page</h1>
+<hr/>
